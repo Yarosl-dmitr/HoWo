09a35a485cdd1df4ec4593108802a0de016b68c5
Yarosl-dmitr <86124496+Yarosl-dmitr@users.noreply.github.com>
2021-12-11 13:46:01 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,13 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
+    }
+
+        public static void printThreeWords (String fruit) {
+            System.out.println(fruit);
+
+        }
+    }
